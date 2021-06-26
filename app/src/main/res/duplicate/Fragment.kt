91af194c63962b4e20a9d1893c45506f5ad3584d
Reen <82717214+MaurineM@users.noreply.github.com>
2021-06-26 18:50:10 +0300

    Uploaded folder

---
+++ app/src/main/res/duplicate/Fragment.kt
@@ -0,0 +1,29 @@
+package com.demo.safeargs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.demo.safeargs.databinding.FragmentABinding
+
+class FragmentA: Fragment() {
+    private var binding : FragmentABinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentABinding.inflate(layoutInflater)
+       return binding.apply {
+
+           binding?.btnShareData?.setOnClickListener {
+               val person = Person("Maurine", 20)
+               val action = FragmentADirections.actionFragmentAToFragmentB(person)
+               findNavController().navigate(action)
+           }
+
+        }?.root
+    }
+}
